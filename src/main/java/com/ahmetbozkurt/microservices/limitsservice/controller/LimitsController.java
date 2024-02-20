e1e7b24d9ee745dd79914aad82cc39cddf467d8f
ahmetbozkurtt <ahmet.bozkurt@canovate.com>
2024-02-20 22:00:33 +0300

    Configuration information retrieval has been added.

--- src/main/java/com/ahmetbozkurt/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/ahmetbozkurt/microservices/limitsservice/controller/LimitsController.java
@@ -1,6 +1,8 @@
 package com.ahmetbozkurt.microservices.limitsservice.controller;
 
 import com.ahmetbozkurt.microservices.limitsservice.bean.Limits;
+import com.ahmetbozkurt.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class LimitsController {
 
     @GetMapping("/limits")
-    public Limits retriveLimits(){
+    public Limits retrieveLimits(){
         return new Limits(1,1000);
+    }
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits-config")
+    public Limits retrieveLimitsConfig(){
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
